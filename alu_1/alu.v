09b6d582af8cb79c17a3d1702eea3571017045ae
Chenglong Tang <tangcl998@gmail.com>
2021-09-13 13:26:20 -0400

    delete ==

--- alu_1/alu.v
+++ alu_1/alu.v
@@ -10,7 +10,7 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	wire [31:0] tmp_operandB;
 	wire [31:0] tmp_notB;
 	my_not my_not_0(data_operandB, tmp_notB);
-	assign tmp_operandB = (ctrl_ALUopcode[0] == 1 ? tmp_notB : data_operandB);
+	assign tmp_operandB = (ctrl_ALUopcode[0] ? tmp_notB : data_operandB);
 	
 	my_csa my_csa_0(data_operandA, tmp_operandB, ctrl_ALUopcode[0], data_result, overflow);
 endmodule
